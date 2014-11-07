c8e050c63eff1114ef2db19415a831a83567685f
Carter Rabasa <carter.rabasa@gmail.com>
2014-11-07 15:48:56 -0800

    0.0.1

--- index.js
+++ index.js
@@ -5,50 +5,55 @@ var os = require('os')
   , twilio = require('twilio')
   , Hapi = require('hapi')
   , Joi = require('joi')
+  , Boom = require('boom')
   , uuid = require('node-uuid')
-  , levelup = require('level')
+  , level = require('level')
   , Caman = require('caman').Caman
   , io;
 
 // valid filters - http://camanjs.com/docs/presets.html
-var filters = {
-  "vintage": "vintage",
-  "lomo": "lomo",
-  "clarity": "clarity",
-  "sincity": "sinCity",
-  "sunrise": "sunrise",
-  "crossprocess": "crossProcess",
-  "orangepeel": "orangePeel",
-  "love": "love",
-  "grungy": "grungy",
-  "jarques": "jarques",
-  "pinhole": "pinhole",
-  "oldboot": "oldBoot",
-  "glowingsun": "glowingSun",
-  "hazydays": "hazyDays",
-  "hermajesty": "herMajesty",
-  "nostalgia": "nostalgia",
-  "hemingway": "hemingway",
-  "concentrate": "concentrate"
-};
+var filters = [
+  "vintage",
+  "lomo",
+  "clarity",
+  "sinCity",
+  "sunrise",
+  "crossProcess",
+  "orangePeel",
+  "love",
+  "grungy",
+  "jarques",
+  "pinhole",  
+  "oldBoot",
+  "glowingSun",
+  "hazyDays",
+  "herMajesty",
+  "nostalgia",
+  "hemingway",
+  "concentrate"
+];
 
-var db = levelup('./mydb', {valueEncoding: "json"});
+var db = level('./mydb', {valueEncoding: "json"});
 
 var client = new twilio.RestClient();
 
 // Create a server with a host and port
 var server = new Hapi.Server(process.env.PORT || 3000);
 
+
 /**
- * Send an MMS with an animated GIF attached
+ * Send an MMS with the filtered photo attached
  */
-var sendPhoto = function(host, photo, from, to) {
+var sendPhoto = function(url_base, photo, from, to) {
   // an assumption made here is that the protocol is HTTP
-  var photoUrl = 'http://' + host + '/' + photo;
+  var photoUrl = url_base + '/' + photo;
+  // update website in realtime
+  io.emit('new_media', photoUrl);
+  // send user the filtered photo via Twilio MMS
   client.sendMessage({
-    to: from, from: to,
+    to: from, from: to, 
     body: 'Powered by Twilio MMS',
-    mediaUrl: photoUrl}, function(err, responseData) {
+    mediaUrl: photoUrl}, function(err, responseData) { 
       if (err) {
         console.log('Error sending MMS: ', JSON.stringify(err));
       }
@@ -58,7 +63,7 @@ var sendPhoto = function(host, photo, from, to) {
 /**
  * Filter and return photo
  */
-var filterAndReply = function(mediaUrl, filter, from, to, host) {
+var applyFilter = function(mediaUrl, filter, from, to, url_base) {
   // create a unique UUID for all of our video/gif processing
   var id = uuid.v1();
 
@@ -83,9 +88,9 @@ var filterAndReply = function(mediaUrl, filter, from, to, host) {
         db.put(id, {filter: filter, number: from, file: filtered});
         // delete the temp file
         fs.unlink(original, function(err) {});
-        sendPhoto(host, filtered, from, to);
+        sendPhoto(url_base, filtered, from, to);
       });
-    });
+    });    
   });
 };
 
@@ -93,9 +98,17 @@ var filterAndReply = function(mediaUrl, filter, from, to, host) {
  * Handle requests for /message
  */
 var handleMessage = function(req, reply) {
+
+  var header = req.headers['x-twilio-signature'];
+  var token = process.env.TWILIO_AUTH_TOKEN;
+  var url_base = 'http://'+req.info.host;
+  if (!twilio.validateRequest(token, header, url_base+'/message', req.payload)) {
+    reply(Boom.forbidden('Invalid x-twilio-signature'));
+    return;
+  }
+
   var from = req.payload.From;
   var to = req.payload.To;
-  var host = req.info.host;
   var mediaUrl = req.payload.MediaUrl0;
   var mediaContentType = req.payload.MediaContentType0;
   var filter = req.payload.Body.toLowerCase().trim();
@@ -103,20 +116,30 @@ var handleMessage = function(req, reply) {
 
   console.log('Processing MMS: ', mediaUrl, mediaContentType, filter);
 
+  // see if a valid filter was passed
+  var filterValid = false;
+  for (i in filters) {
+    if (filter === filters[i].toLowerCase()) {
+      filterValid = true;
+      filter = filters[i];
+      break;
+    }
+  }
+ 
   // check to see that the user has submitted an image
   if (mediaUrl && mediaContentType && mediaContentType.indexOf('image') >= 0) {
     // check to see that a valid filter command was passed
-    if (filter === null || (Object.keys(filters).indexOf(filter) >= 0)) {
+    if (filterValid) {
       // send immediate reply
       twiml.message('Thanks for the awesome photo! Applying filter now..');
       reply(twiml.toString()).type('text/xml');
-
-      filterAndReply(mediaUrl, filters[filter], from, to, host);
+      
+      applyFilter(mediaUrl, filter, from, to, url_base);
     }
     else {
       // else respond with a list of valid filters
-      twiml.message('Hmmm, I do not recognize the filter "'+ filter + '".\n\n' +
-        'Valid filters are: ' + Object.keys(filters).join(', '));
+      twiml.message('Hmmm, I do not recognize the filter "'+ filter + '".\n\n' +  
+        'Valid filters are: ' + filters.join(', '));
       reply(twiml.toString()).type('text/xml');
     }
 
@@ -126,7 +149,7 @@ var handleMessage = function(req, reply) {
     twiml.message('Thanks for trying Phonestagram, the photo filtering ' +
       'and sharing app that works on any phone! Just text a photo to this ' +
       'number and specify the filter you would like.\n\nValid filters are: ' +
-      Object.keys(filters).join(', '));
+      filters.join(', '));
     reply(twiml.toString()).type('text/xml');
   }
 };
@@ -137,10 +160,11 @@ var twilioRequestSchema = Joi.object().keys({
 }).unknown();
 
 // Add the routes
+
 server.route([{
   method: 'POST', path: '/message', handler: handleMessage, config: {
     validate: {
-      payload: twilioRequestSchema
+      payload: twilioRequestSchema,
     }
   } }, {
   method: 'GET', path: '/{p*}',
@@ -156,9 +180,10 @@ server.start(function () {
 
   io.on('connection', function(socket){
     io.to(socket.id).emit('connected', 'Connected!');
-    streamImagesToNewUser(socket.id);
   });
 
   console.log("Listening on port", process.env.PORT || 3000);
 });
+
+
 
